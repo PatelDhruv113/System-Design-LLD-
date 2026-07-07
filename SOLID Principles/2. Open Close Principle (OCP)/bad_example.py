aebf1f486b3fd316139f06fbf0c83df064ca5d19
Dhruv Patel <pateldhruv0113@gmail.com>
2026-07-07 11:06:53 +0530

    OCP implemented

--- SOLID Principles/2. Open Close Principle (OCP)/bad_example.py
+++ SOLID Principles/2. Open Close Principle (OCP)/bad_example.py
@@ -1,2 +1,16 @@
 class PaymentProcessor:
-    def 
+    def pay(self, payment_method: str, amount: int):
+        if payment_method == "UPI":
+            print(f"Starting UPI transaction of Rs.{amount}")
+            print(f"UPI transaction finisheed")
+        elif payment_method == "credit_card":
+            print(f"Starting Credit card transaction of Rs.{amount}")
+            print(f"Credit card transaction finisheed")
+        elif payment_method == "net_banking":
+            print(f"Starting Net banking transaction of Rs.{amount}")
+            print(f"Net banking transaction finisheed")
+        else:
+            print("Payment method was not found")
+
+pay_p = PaymentProcessor()
+pay_p.pay("credit_card",500)
