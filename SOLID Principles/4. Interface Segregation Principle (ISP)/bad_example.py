85b3c78731ad77e1e07e04ec3157aca30ac2a7fd
Dhruv Patel <pateldhruv0113@gmail.com>
2026-07-07 14:25:06 +0530

    bad and good example for ISP

---
+++ SOLID Principles/4. Interface Segregation Principle (ISP)/bad_example.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+
+class Employee(ABC):
+    @abstractmethod
+    def eat(self):
+        pass
+
+    @abstractmethod
+    def work(self):
+        pass
+
+class Worker(Employee):
+    def eat(self):
+        print("Worker is eating")
+    
+    def work(self):
+        print("Worker is working")
+    
+class Robot(Employee):
+    def work(self):
+        print("Robot is working")
+    
+    def eat(self):
+        raise Exception("Robot can't eat")
+
+r = Robot()
+r.eat()
---
+++ SOLID Principles/4. Interface Segregation Principle (ISP)/good_example.py
@@ -0,0 +1,30 @@
+from abc import ABC, abstractmethod
+
+class Workable(ABC):
+    @abstractmethod
+    def work(self):
+        pass
+
+class Eatable(ABC):
+    @abstractmethod
+    def eat(self):
+        pass
+
+class Robot(Workable):
+    def work(self):
+        print("Robot is working")
+    
+class Employee(Workable, Eatable):
+    def eat(self):
+        print("Employee is eating")
+    
+    def work(self):
+        print("Employeee is working")
+
+
+# e = Employee()
+# e.eat()
+# e.work()
+
+r = Robot()
+r.work()
